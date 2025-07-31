c4e412a84dbac50eb38aa07d913d632e9dff6e2a
lavasurfing <ashishhansdah0@gmail.com>
2025-07-30 23:45:40 -0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
+export default function Home() {
+  return (
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-[300rem] p-8 pb-20 gap-16 sm:p-20">
+      <h2>Portfolio</h2>
+    </div>
+  );
+}
