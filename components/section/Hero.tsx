c2d131ba241b37a61d9573d70e235ef7185f5274
lavasurfing <ashishhansdah0@gmail.com>
2025-08-04 17:45:28 +0530

    Hero section naming and font adjust done

--- components/section/Hero.tsx
+++ components/section/Hero.tsx
@@ -2,7 +2,10 @@ import { WavyBackground } from "../ui/wavy-background"
 const Hero = () => {
     return (
     <WavyBackground className="max-w-4xl mx-auto pb-40">
-        <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center">
+                <p className="text-2xl md:text-4xl lg:text-7xl text-white font-bold inter-var text-center pb-4 mb-30">
+            Ashish Hansdah
+        </p>
+        <p className="text-2xl md:text-4xl lg:text-7xl text-gray  font-bold inter-var text-center">
             Full Stack Developer
         </p>
         <p className="text-base md:text-lg mt-4 text-white font-normal inter-var text-center">
